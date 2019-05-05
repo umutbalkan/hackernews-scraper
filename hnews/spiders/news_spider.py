497a1f265bc8eee3b842ddc816d7c6d17b2cd89d
Umut Balkan <umutbalkan@icloud.com>
2019-05-05 16:37:03 +0300

    tinkering with crawler

--- hnews/spiders/news_spider.py
+++ hnews/spiders/news_spider.py
@@ -4,12 +4,16 @@ import scrapy
 class HNewsSpider(scrapy.Spider):
     name = "spidey"
     start_urls = [
-        'http://news.ycombinator.com',
+        'https://news.ycombinator.com/',
     ]
 
     def parse(self, response):
-        for item in response.css('td.title'):
+        for item in response.css('tr.athing'):
             yield {
                 'text': item.css('a.storylink::text').get(),
                 'link': item.css('a.storylink::attr(href)').get(),
             }
+        next_page = response.css('td.title a.morelink::attr(href)').get()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
